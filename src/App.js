74c88ef7eef22c4420a858bf871d66d9e6fadfae
Tyler Rasor <tyler.rasor@cardinalhealth.com>
2020-01-03 22:16:57 -0500

    does a thing where you can pick a file or drag and drop one and then jams it onto the page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useCallback, useState } from 'react'
+import { useDropzone } from 'react-dropzone'
+import styled from 'styled-components'
+import { Upload } from 'styled-icons/feather/Upload'
 
-function App() {
+const Container = styled.div`
+  height: 100px;
+  background: #ababab;
+  text-justify: auto;
+  text-align: center;
+  padding-top: 20px;
+  border-radius: 10px;
+  margin-top: 80px;
+`
+
+const StyledUploadIcon = styled(Upload).attrs(props => ({
+  ...props,
+  size: 24
+}))`
+
+`
+
+let Dropzone = props => {
+  const onDrop = useCallback(acceptedFiles => {
+    props.jamImageOnPage(acceptedFiles)
+  }, [])
+  const {getRootProps, getInputProps, isDragActive} = useDropzone({onDrop})
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Container {...getRootProps()}>
+      <input {...getInputProps()} />
+      {
+        isDragActive ?
+          <p>Get the party started...</p> :
+          <p>Drag and drop or click here to get the party started...</p>
+      }
+      <StyledUploadIcon/>
+    </Container>
+  )
 }
 
-export default App;
+const StyledAppWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+`
+
+const StyledColumnWrapper = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+`
+
+const StyledPartyJammingSpace = styled.div`
+  height: 200px;
+  background: #ababab;
+  text-justify: auto;
+  text-align: center;
+  padding-top: 20px;
+  border-radius: 10px;
+  margin-top: 80px;
+`
+
+const App = () => {
+  const [party, setParty] = useState()
+  const jamImageOnPage = image => { setParty(<div><img src={URL.createObjectURL(image[0])}/></div>); console.log(party) }
+
+  return (
+    <StyledAppWrapper>
+      <StyledColumnWrapper>
+        <Dropzone jamImageOnPage={jamImageOnPage}/>
+        <StyledPartyJammingSpace>
+          {party}
+        </StyledPartyJammingSpace>
+      </StyledColumnWrapper>
+    </StyledAppWrapper>
+  )
+}
+
+export default App
